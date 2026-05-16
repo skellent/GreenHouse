cdfe8e176b537ef2206531ac04c23d88c80c1c34
Skellent <skellent69@gmail.com>
2026-05-15 23:09:55 -0400

    Inicio de Reconfiguracion

---
+++ core/config.h
@@ -0,0 +1,26 @@
+#ifndef CONFIG
+
+#define CONFIG
+#include <Arduino.h>
+
+/* CÁMARA */
+#define CAM_PWDN  -1
+#define CAM_RESET -1
+// ------------------
+#define CAM_XCLK 15
+#define CAM_SIOD  4
+#define CAM_SIOC  5
+// ------------------
+#define CAM_Y9    16
+#define CAM_Y8    17
+#define CAM_Y7    18
+#define CAM_Y6    12
+#define CAM_Y5    10
+#define CAM_Y4     8
+#define CAM_Y3     9
+#define CAM_Y2    11
+#define CAM_VSYNC  6
+#define CAM_HREF   7
+#define CAM_PCLK  13
+
+#endif
---
+++ core/core.ino
@@ -0,0 +1,14 @@
+#include "esp_camera.h"
+
+#include "config.h"
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
